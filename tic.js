dda5ce7fe8d835c2748f24ba5402f5592461e03d
AsadNazir <79693635+AsadNazir@users.noreply.github.com>
2021-10-19 17:23:23 +0500

    2nd Release only JS
    Added the draw functionality and checking for already filled boxes

--- tic.js
+++ tic.js
@@ -2,25 +2,43 @@ let Tictac = ['X', 'O'];
 let count = 0;
 let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 let boxes = document.querySelectorAll(`.box`);
+
+//This here Adds an event listner to box 
 boxes.forEach(function(ele, index) {
     ele.addEventListener("click", function() {
-        ele.innerHTML = `${Tictac[count]}`;
-        arr[index] = Tictac[count];
-        count++;
+        if (arr[index] != 'X' && arr[index] != 'O') {
 
-        count = count % 2;
+            ele.innerHTML = `${Tictac[count]}`;
+            arr[index] = Tictac[count];
+            count++;
+            count = count % 2;
+        }
         console.log(arr);
         setTimeout(checker, 500);
     })
 
 });
-
+//This function resets the board when game is done
 function Reset() {
     boxes.forEach(function(ele) {
         ele.innerHTML = ` `;
     })
     arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
     alert("Game Ended");
+}
+//This checks for winner
+function DrawChecker() {
+    let type = false;
+    for (let i = 0; i < arr.length; i++) {
+
+        if (typeof(arr[i]) != "string") {
+            type = true;
+            break;
+        }
+    }
+    if (!type) {
+        Reset();
+    }
 }
 
 function checker() {
@@ -47,5 +65,8 @@ function checker() {
     }
     if (arr[2] === arr[4] && arr[4] === arr[6]) {
         Reset();
+    } else {
+        DrawChecker();
     }
+
 }
