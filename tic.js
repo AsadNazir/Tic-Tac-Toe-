0488a136a328f4481a48b9fcbf41dc8f189cce92
AsadNazir <79693635+AsadNazir@users.noreply.github.com>
2021-11-13 19:56:12 +0500

    Almost Final Version of tic tac toe project

--- tic.js
+++ tic.js
@@ -2,12 +2,14 @@ let Tictac = ['X', 'O'];
 let count = 0;
 let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 let boxes = document.querySelectorAll(`.box`);
+
 //Modal Box Related variables
-let Click_me = document.querySelector(`#Select`);
-let IsModalOn = true;
-Click_me.addEventListener('click', modal_btn)
-    //Calling ShowModal Fucntion
-show_modal();
+
+// let IsModalOn = true;
+
+
+//Calling ShowModal Fucntion
+show_modal(`Choose`);
 
 //This here Adds an event listner to box 
 boxes.forEach(function(ele, index) {
@@ -20,19 +22,23 @@ boxes.forEach(function(ele, index) {
             count = count % 2;
         }
         console.log(arr);
-        setTimeout(checker, 200);
+        //Delay so that user can see where it is marking
+        setTimeout(checker, 100);
     })
 
 });
+
 //This function resets the board when game is done
 function Reset() {
     boxes.forEach(function(ele) {
         ele.innerHTML = ` `;
     })
     arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-    //alert("Game Ended");
-    show_modal();
+
+    //Passin the message
+    show_modal(arguments[0]);
 }
+
 //This checks for winner
 function DrawChecker() {
     let type = false;
@@ -50,52 +56,60 @@ function DrawChecker() {
 
 function checker() {
     if (arr[0] === arr[1] && arr[1] === arr[2]) {
-        Reset(arr[0]);
+        Reset(`${arr[0]} won`);
     }
     if (arr[3] === arr[4] && arr[4] === arr[5]) {
-        Reset(arr[3]);
+        Reset(`${arr[3]} won`);
     }
     if (arr[6] === arr[7] && arr[7] === arr[8]) {
-        Reset(arr[6]);
+        Reset(`${arr[6]} won`);
     }
     if (arr[0] === arr[3] && arr[3] === arr[6]) {
-        Reset(arr[0]);
+        Reset(`${arr[0]} won`);
     }
     if (arr[1] === arr[4] && arr[4] === arr[7]) {
-        Reset(arr[1]);
+        Reset(`${arr[1]} won`);
     }
     if (arr[2] === arr[5] && arr[5] === arr[8]) {
-        Reset(arr[2]);
+        Reset(`${arr[2]} won`);
     }
     if (arr[0] === arr[4] && arr[4] === arr[8]) {
-        Reset(arr[0]);
+        Reset(`${arr[0]} won`);
     }
     if (arr[2] === arr[4] && arr[4] === arr[6]) {
-        Reset(arr[2]);
+        Reset(`${arr[2]} won`);
     } else {
         DrawChecker();
     }
 
 }
+//Reset button
+let reset = document.querySelector(`#reset_btn`);
+reset.addEventListener('click', function() {
+    Reset(`Choose`);
+})
 
 //Modal Box JavaSript
 
-
-
 function modal_btn() {
+    count = arguments[0];
     let background = document.querySelector(`.tic`);
-    background.style.opacity = `1`;
+    background.style.filter = `blur(0px)`;
     let Modal = document.querySelector(`.modal`);
+    Modal.style.transfrom = `translateX(-50%) translateY(-50%) scale(0.3)`;
     Modal.style.display = `none`;
     IsModalOn = false;
 
 }
+// filter: blur(8px);
+// -webkit-filter: blur(8px);
 //For Showing Modal
 function show_modal() {
     let background = document.querySelector(`.tic`);
-    background.style.opacity = `0.5`;
+    background.style.filter = `blur(8px)`;
     let Modal = document.querySelector(`.modal`);
-    Modal.style.display = `grid`;
-
-
+    let text = document.querySelector(`#Modal_text`);
+    text.innerHTML = `${arguments[0]}`;
+    Modal.style.display = `flex`;
+    Modal.style.transform = `translateX(-50%) translateY(-50%) scale(1)`;
 }
